67be2c854b5fc07c2e7d2fb8b9a55af09f78842c
Aymeric <counteraccro@gmail.com>
2021-11-15 14:20:13 +0100

    Update FaqQuestionAnswerTag.php

--- src/Entity/Module/FAQ/FaqQuestionAnswerTag.php
+++ src/Entity/Module/FAQ/FaqQuestionAnswerTag.php
@@ -21,13 +21,13 @@ class FaqQuestionAnswerTag
     private $id;
 
     /**
-     * @ORM\ManyToOne(targetEntity=FaqQuestionAnswer::class, inversedBy="Tags", cascade={"persist"})
+     * @ORM\ManyToOne(targetEntity=FaqQuestionAnswer::class, inversedBy="faqQuestionAnswerTags")
      * @ORM\JoinColumn(nullable=false)
      */
     private $FaqQuestionAnswer;
 
     /**
-     * @ORM\ManyToOne(targetEntity=Tag::class, inversedBy="faqQuestionAnswerTags", cascade={"persist"})
+     * @ORM\ManyToOne(targetEntity=Tag::class, inversedBy="faqQuestionAnswerTags")
      * @ORM\JoinColumn(nullable=false)
      */
     private $tag;
