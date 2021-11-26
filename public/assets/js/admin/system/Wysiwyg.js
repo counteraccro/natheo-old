49043fbe60f6b37d6e17e66357fa24ff4ba52823
Aymeric <counteraccro@gmail.com>
2021-11-26 08:27:46 +0100

    Editeur Wysiwyg
    Mise en place éditeur de texte

---
+++ public/assets/js/admin/system/Wysiwyg.js
@@ -0,0 +1,16 @@
+
+/**
+ *  JS pour l'éditeur de texte
+ *  @author Gourdon Aymeric
+ *  @version 1.0
+ **/
+let Wysiwyg = {}
+
+/**
+ * Charge l'éditeur standard sans options
+ * @param id
+ * @constructor
+ */
+Wysiwyg.DefaultEditor = function (id) {
+    $(id).summernote();
+}
